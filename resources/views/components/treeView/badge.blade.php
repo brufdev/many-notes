0987bd580baec1259aef332cc47463d6b04c9050
brufdev <brufdev@proton.me>
2025-05-16 14:55:35 +0100

    Adjust colors and accept attributes

--- resources/views/components/treeView/badge.blade.php
+++ resources/views/components/treeView/badge.blade.php
@@ -1,3 +1,6 @@
-<span class="p-1 text-xs font-medium rounded bg-light-base-100 dark:bg-base-800 text-light-base-950 dark:text-base-50">
+<span
+    {{ $attributes }}
+    class="p-1 text-xs font-medium rounded bg-light-base-400 dark:bg-base-500 text-light-base-950 dark:text-base-50"
+>
     {{ $slot }}
 </span>
