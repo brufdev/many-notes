70f15cbc259917ceb8005821bcef733fe9c62b64
brufdev <brufdev@proton.me>
2025-06-18 14:43:03 +0100

    Fix template header and web app manifest

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,13 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content">
     <title>{{ $title ?? 'Many Notes' }}</title>
-    <link rel="icon" type="image/png" href="/assets/favicon-16x16.png" sizes="16x16" />
-    <link rel="icon" type="image/png" href="/assets/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg" />
+    <link rel="icon" type="image/png" href="/assets/icon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="/assets/icon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/svg+xml" href="/assets/icon.svg" sizes="any" />
     <link rel="shortcut icon" href="/assets/favicon.ico" />
-    <link rel="apple-touch-icon" href="/assets/apple-touch-icon.png" sizes="180x180" />
+    <link rel="apple-touch-icon" href="/assets/icon-180x180.png" sizes="180x180" />
     <meta name="apple-mobile-web-app-title" content="Many Notes" />
-    <link rel="manifest" href="/assets/site.webmanifest" />
+    <link rel="manifest" href="/site.webmanifest" />
 
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
