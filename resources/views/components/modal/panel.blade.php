6fdc8657a62c3708c7565a7f609f915e7aadd9a9
brufdev <brufdev@proton.me>
2025-06-21 11:56:59 +0100

    Fix modals scroll

--- resources/views/components/modal/panel.blade.php
+++ resources/views/components/modal/panel.blade.php
@@ -4,27 +4,29 @@
 
 <template x-teleport="body">
     <div
-        class="fixed inset-0 z-50 w-full overflow-x-hidden overflow-y-auto max-h-full flex justify-center {{ $top ? 'sm:mt-5' : 'items-end sm:items-center' }}"
+        class="relative z-50"
         x-show="modalOpen"
         @keydown.escape.window="modalOpen = false"
     >
         <div class="fixed inset-0 opacity-50 bg-base-950"></div>
-        <div class="relative w-full max-w-2xl">
-            <div
-                x-trap="modalOpen"
-                @click.outside="modalOpen = false"
-            >
-                <div class="flex justify-between p-6 pb-0 rounded-t-lg bg-light-base-200 dark:bg-base-950 text-light-base-950 dark:text-base-50">
-                    <h3 class="text-lg">{{ $title }}</h3>
-                    <x-modal.close class="flex items-center">
-                        <button type="button">
-                            <span class="sr-only">{{ __('Close') }}</span>
-                            <x-icons.xMark class="w-5 h-5" />
-                        </button>
-                    </x-modal.close>
-                </div>
-                <div class="p-6 shadow-lg sm:rounded-b-lg bg-light-base-200 dark:bg-base-950">
-                    {{ $slot }}
+        <div class="fixed inset-0 overflow-x-hidden overflow-y-auto flex justify-center {{ $top ? 'sm:mt-5' : 'items-end sm:items-center' }}">
+            <div class="relative w-full max-w-2xl max-h-full">
+                <div
+                    x-trap="modalOpen"
+                    @click.outside="modalOpen = false"
+                >
+                    <div class="flex justify-between p-6 pb-0 rounded-t-lg bg-light-base-200 dark:bg-base-950 text-light-base-950 dark:text-base-50">
+                        <h3 class="text-lg">{{ $title }}</h3>
+                        <x-modal.close class="flex items-center">
+                            <button type="button">
+                                <span class="sr-only">{{ __('Close') }}</span>
+                                <x-icons.xMark class="w-5 h-5" />
+                            </button>
+                        </x-modal.close>
+                    </div>
+                    <div class="p-6 shadow-lg sm:rounded-b-lg bg-light-base-200 dark:bg-base-950">
+                        {{ $slot }}
+                    </div>
                 </div>
             </div>
         </div>
