df8d7340851126760f92bc7b7d9747b5560d8ba9
brufdev <brufdev@proton.me>
2025-07-11 19:58:43 +0100

    Update application icons

--- resources/views/components/icons/bell.blade.php
+++ resources/views/components/icons/bell.blade.php
@@ -1,6 +1,6 @@
-<svg {{ $attributes }} data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor"
+<svg {{ $attributes }} fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10.268 21a2 2 0 0 0 3.464 0" />
     <path
-        d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-        stroke-linecap="round" stroke-linejoin="round"></path>
+        d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326" />
 </svg>
--- resources/views/components/icons/lock.blade.php
+++ resources/views/components/icons/lock.blade.php
@@ -1,6 +1,5 @@
 <svg {{ $attributes }} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
     stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="12" cy="12" r="10" />
-    <path d="M12 16v-4" />
-    <path d="M12 8h.01" />
+    <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
+    <path d="M7 11V7a5 5 0 0 1 10 0v4" />
 </svg>
