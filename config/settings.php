6bc25a01cbd541da6744fa974245ce63725cdb4f
brufdev <brufdev@proton.me>
2025-07-25 19:24:09 +0100

    Add configuration for toggling local authentication

--- config/settings.php
+++ config/settings.php
@@ -4,4 +4,17 @@ declare(strict_types=1);
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Local Authentication
+    |--------------------------------------------------------------------------
+    |
+    | This option defines if the local authentication feature is enabled or not.
+    |
+    */
+
+    'local_auth' => [
+        'enabled' => env('SETTINGS_LOCAL_AUTH_ENABLED', true),
+    ],
+
 ];
