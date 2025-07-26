61ee8cda29c059cd6fd917f4888e92f1c934a42e
brufdev <brufdev@proton.me>
2025-07-26 12:29:02 +0100

    Add URL configuration to redirect a user after logging out

--- config/services.php
+++ config/services.php
@@ -35,6 +35,7 @@ return [
         'client_secret' => env('AUTHELIA_CLIENT_SECRET'),
         'redirect' => env('AUTHELIA_REDIRECT_URI'),
         'base_url' => env('AUTHELIA_BASE_URL'),
+        'post_logout_redirect_uri' => env('AUTHELIA_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'authentik' => [
@@ -42,36 +43,42 @@ return [
         'client_secret' => env('AUTHENTIK_CLIENT_SECRET'),
         'redirect' => env('AUTHENTIK_REDIRECT_URI'),
         'base_url' => env('AUTHENTIK_BASE_URL'),
+        'post_logout_redirect_uri' => env('AUTHENTIK_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'bitbucket' => [
         'client_id' => env('BITBUCKET_CLIENT_ID'),
         'client_secret' => env('BITBUCKET_CLIENT_SECRET'),
         'redirect' => env('BITBUCKET_REDIRECT_URI'),
+        'post_logout_redirect_uri' => env('BITBUCKET_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => env('FACEBOOK_REDIRECT_URI'),
+        'post_logout_redirect_uri' => env('FACEBOOK_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
         'redirect' => env('GITHUB_REDIRECT_URI'),
+        'post_logout_redirect_uri' => env('GITHUB_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'gitlab' => [
         'client_id' => env('GITLAB_CLIENT_ID'),
         'client_secret' => env('GITLAB_CLIENT_SECRET'),
         'redirect' => env('GITLAB_REDIRECT_URI'),
+        'post_logout_redirect_uri' => env('GITLAB_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI'),
+        'post_logout_redirect_uri' => env('GOOGLE_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'keycloak' => [
@@ -80,12 +87,14 @@ return [
         'redirect' => env('KEYCLOAK_REDIRECT_URI'),
         'base_url' => env('KEYCLOAK_BASE_URL'),
         'realms' => env('KEYCLOAK_REALM'),
+        'post_logout_redirect_uri' => env('KEYCLOAK_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'linkedin' => [
         'client_id' => env('LINKEDIN_CLIENT_ID'),
         'client_secret' => env('LINKEDIN_CLIENT_SECRET'),
         'redirect' => env('LINKEDIN_REDIRECT_URI'),
+        'post_logout_redirect_uri' => env('LINKEDIN_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'slack' => [
@@ -96,12 +105,14 @@ return [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
+        'post_logout_redirect_uri' => env('SLACK_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'twitter' => [
         'client_id' => env('TWITTER_CLIENT_ID'),
         'client_secret' => env('TWITTER_CLIENT_SECRET'),
         'redirect' => env('TWITTER_REDIRECT_URI'),
+        'post_logout_redirect_uri' => env('TWITTER_POST_LOGOUT_REDIRECT_URI'),
     ],
 
     'zitadel' => [
