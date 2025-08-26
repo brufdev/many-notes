8f099af1ca1f19d63a399fd26a08995a4cae0908
m42cel <m42cel@users.noreply.github.com>
2025-08-26 18:18:53 +0200

    Use Tailwind css classes instead of a custom css class

--- resources/views/components/treeView/items.blade.php
+++ resources/views/components/treeView/items.blade.php
@@ -4,7 +4,7 @@
     @if ($root)
         class="relative w-full"
     @else
-        class="relative w-full tree-view-directory"
+        class="relative w-full pl-2 ml-2 border-l-2 border-light-base-400 dark:border-base-500"
         x-show="accordionOpen"
         x-collapse
         x-cloak
