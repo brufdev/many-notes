4a5a5b9e41b2a3c74a5434e3b73e7fc8959ac8dd
brufdev <brufdev@proton.me>
2025-09-26 11:33:52 +0100

    Prevent parsing tags inside inline codes and code blocks

--- app/Actions/ProcessVaultNodeTags.php
+++ app/Actions/ProcessVaultNodeTags.php
@@ -19,18 +19,41 @@ final readonly class ProcessVaultNodeTags
 
         /** @var string $content */
         $content = $node->content;
-        preg_match_all('/#[\p{L}0-9_-]+/u', $content, $matches, PREG_OFFSET_CAPTURE);
 
-        if ($matches[0] === []) {
+        // Find tags
+        preg_match_all('/(?:^|\s)(#[\p{L}0-9_-]+)/u', $content, $matches, PREG_OFFSET_CAPTURE);
+
+        if ($matches[1] === []) {
             return;
         }
 
-        foreach ($matches[0] as $match) {
+        $allTags = $matches[1];
+
+        // Find inline codes and codeblocks start and end positions
+        preg_match_all('/`[^`\n]+`|```[^(?:```)]+```/u', $content, $matches, PREG_OFFSET_CAPTURE);
+        $codePositions = array_map(
+            fn(array $match): array => [
+                'start' => $match[1],
+                'end' => $match[1] + mb_strlen($match[0]),
+            ],
+            $matches[0],
+        );
+
+        // Filter out tags that are inside code blocks
+        $filteredTags = array_filter(
+            $allTags,
+            fn(array $tag): bool => array_all(
+                $codePositions,
+                fn(array $position): bool => $tag[1] < $position['start'] || $tag[1] > $position['end'],
+            ),
+        );
+
+        foreach ($filteredTags as $filteredTag) {
             $tag = Tag::firstOrCreate([
-                'name' => mb_substr($match[0], 1),
+                'name' => mb_substr($filteredTag[0], 1),
             ]);
 
-            $node->tags()->attach($tag->id, ['position' => $match[1]]);
+            $node->tags()->attach($tag->id, ['position' => $filteredTag[1]]);
         }
     }
 }
