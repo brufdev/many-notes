9588112551ecae3ad293b015c3ded375cd632338
brufdev <brufdev@proton.me>
2025-10-07 19:29:36 +0100

    Fix checkbox UI on light theme

--- resources/views/components/form/checkbox.blade.php
+++ resources/views/components/form/checkbox.blade.php
@@ -1,6 +1,6 @@
 <label class="inline-flex items-center text-base font-medium">
     <input
-        class="rounded shadow-sm border-light-base-300 dark:border-base-500 bg-light-base-100 dark:bg-base-800 text-primary-600 focus:ring-0 focus:ring-offset-0 focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 focus-visible:outline-light-base-600 dark:focus-visible:outline-base-400"
+        class="rounded shadow-sm bg-light-base-100 dark:bg-base-800 checked:bg-primary-400 dark:checked:bg-primary-500 border-light-base-300 dark:border-base-500 focus:ring-0 focus:ring-offset-0 focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 focus-visible:outline-light-base-600 dark:focus-visible:outline-base-400"
         type="checkbox"
         wire:model="{{ $name }}"
     />
