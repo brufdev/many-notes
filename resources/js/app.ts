e48468d978e973506a953a00d36c489cfb918f52
Bruno <brufdev@proton.me>
2025-10-30 21:16:33 +0000

    Fix linting and formatting issues

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,17 +1,18 @@
-import './bootstrap.js';
-import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
+import { createApp, h } from 'vue';
+import './bootstrap.js';
 
 createInertiaApp({
-    resolve: name => resolvePageComponent(
-        `./pages/${name}.vue`,
-        import.meta.glob<DefineComponent>('./pages/**/*.vue'),
-    ),
+    resolve: name =>
+        resolvePageComponent(
+            `./pages/${name}.vue`,
+            import.meta.glob<DefineComponent>('./pages/**/*.vue')
+        ),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .mount(el);
+        const app = createApp(h(App, props));
+        app.use(plugin);
+        app.mount(el);
     },
 });
