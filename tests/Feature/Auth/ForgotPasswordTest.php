55ddca7b97f453dfff6421850cd73b87f8dd869c
Bruno <brufdev@proton.me>
2025-11-03 01:15:12 +0000

    Migrate ForgotPassword feature tests

--- tests/Feature/Auth/ForgotPasswordTest.php
+++ tests/Feature/Auth/ForgotPasswordTest.php
@@ -2,27 +2,30 @@
 
 declare(strict_types=1);
 
-use App\Livewire\Auth\ForgotPassword;
 use App\Models\User;
-use Livewire\Livewire;
 
 it('returns a successful response', function (): void {
-    Livewire::test(ForgotPassword::class)
-        ->assertStatus(200);
+    $response = $this->get(route('forgot.password'));
+
+    $response->assertStatus(200);
 });
 
 it('sends a password reset link', function (): void {
     $user = User::factory()->create();
 
-    Livewire::test(ForgotPassword::class)
-        ->set('form.email', $user->email)
-        ->call('send')
-        ->assertSee('We have emailed your password reset link');
+    $response = $this->post(route('forgot.password'), [
+        'email' => $user->email,
+    ]);
+
+    $response->assertSessionHas('status', 'We have emailed your password reset link.');
 });
 
 it('fails sending a password reset link', function (): void {
-    Livewire::test(ForgotPassword::class)
-        ->set('form.email', 'invalid@email.com')
-        ->call('send')
-        ->assertSee('We can\'t find a user with that email address');
+    $response = $this->post(route('forgot.password'), [
+        'email' => fake()->email(),
+    ]);
+
+    $response->assertSessionHasErrors([
+        'email' => 'We can\'t find a user with that email address.',
+    ]);
 });
