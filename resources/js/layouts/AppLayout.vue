8211ad78f7b9adf0ee944ebec62b838a780c24bf
Bruno <brufdev@proton.me>
2025-11-27 12:31:57 +0000

    Add application layout

---
+++ resources/js/layouts/AppLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    document.querySelector('#app')?.classList.add('flex', 'flex-col', 'h-dvh');
+});
+</script>
+
+<template>
+    <slot />
+</template>
