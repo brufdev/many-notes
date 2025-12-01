f17da957be223d932e7519d5ebd519468212227a
Bruno <brufdev@proton.me>
2025-12-01 22:12:36 +0000

    Add support for small submit buttons

--- resources/js/components/form/Submit.vue
+++ resources/js/components/form/Submit.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
+import PrimaryButton from '@/components/ui/PrimaryButton.vue';
 import Spinner from '@/icons/Spinner.vue';
 
 defineProps<{
     label: string;
+    fullWidth?: boolean;
     processing: boolean;
 }>();
 </script>
 
 <template>
-    <button
-        class="bg-primary-400 dark:bg-primary-500 hover:bg-primary-300 dark:hover:bg-primary-600 text-light-base-50 focus-visible:outline-light-base-600 dark:focus-visible:outline-base-400 relative flex w-full justify-center rounded-md px-3 py-1.5 text-sm leading-6 font-semibold shadow-sm focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 disabled:cursor-not-allowed disabled:opacity-75"
-        type="submit"
-    >
-        {{ label }}
+    <PrimaryButton :full-width type="submit">
+        <span :class="!fullWidth && processing ? 'opacity-0' : ''">{{ label }}</span>
 
-        <span v-if="processing" class="absolute top-0 right-0 bottom-0 flex items-center pr-4">
+        <span
+            v-if="processing"
+            class="absolute top-0 bottom-0 flex items-center"
+            :class="fullWidth ? 'right-4' : ''"
+        >
             <Spinner class="h-5 w-5 animate-spin" />
         </span>
-    </button>
+    </PrimaryButton>
 </template>
