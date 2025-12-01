6c6fc973fc1dd010867f74f6a1ce5651e3675ca0
Bruno <brufdev@proton.me>
2025-12-01 23:40:11 +0000

    Remove unnecessary fullWidth property

--- resources/js/components/ui/PrimaryButton.vue
+++ resources/js/components/ui/PrimaryButton.vue
@@ -1,13 +1,6 @@
-<script setup lang="ts">
-defineProps<{
-    fullWidth?: boolean;
-}>();
-</script>
-
 <template>
     <button
-        class="bg-primary-400 dark:bg-primary-500 hover:bg-primary-300 dark:hover:bg-primary-600 text-light-base-50 focus-visible:outline-light-base-600 dark:focus-visible:outline-base-400 relative flex justify-center rounded-md px-3 py-1.5 text-sm leading-6 font-semibold shadow-sm focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 disabled:cursor-not-allowed disabled:opacity-75"
-        :class="fullWidth ? 'w-full' : ''"
+        class="bg-primary-400 dark:bg-primary-500 hover:bg-primary-300 dark:hover:bg-primary-600 text-light-base-50 focus-visible:outline-light-base-600 dark:focus-visible:outline-base-400 relative flex justify-center rounded-md px-4 py-1.5 text-sm leading-6 font-semibold shadow-sm focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 disabled:cursor-not-allowed disabled:opacity-75"
     >
         <slot />
     </button>
