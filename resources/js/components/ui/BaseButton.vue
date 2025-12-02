4b51151bd96989f416f50005ddef714c8f20269b
Bruno <brufdev@proton.me>
2025-12-02 11:47:41 +0000

    Add default type attribute to buttons

--- resources/js/components/ui/BaseButton.vue
+++ resources/js/components/ui/BaseButton.vue
@@ -1,7 +1,17 @@
+<script setup lang="ts">
+interface Props {
+    type?: 'button' | 'reset' | 'submit';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    type: 'button',
+});
+</script>
+
 <template>
     <button
-        type="button"
         class="focus-visible:ring-light-base-600 dark:focus-visible:ring-base-400 rounded focus:outline-none focus-visible:ring-1"
+        :type="props.type"
     >
         <slot />
     </button>
