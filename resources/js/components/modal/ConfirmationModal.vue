10d3096e21885e1ee629d28b87d8880f865fdffb
Bruno <brufdev@proton.me>
2025-12-03 00:41:29 +0000

    Adjust confirmation modal for generic actions

--- resources/js/components/modal/ConfirmationModal.vue
+++ resources/js/components/modal/ConfirmationModal.vue
@@ -1,22 +1,31 @@
 <script setup lang="ts">
 import Submit from '@/components/form/Submit.vue';
+import SecondaryButton from '@/components/ui/SecondaryButton.vue';
 import { useModalManager } from '@/composables/useModalManager';
 import { useToast } from '@/composables/useToast';
 import { RouteFormDefinition } from '@/wayfinder';
-import { Form } from '@inertiajs/vue3';
-import SecondaryButton from '../ui/SecondaryButton.vue';
+import { Form, usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
 const { closeModal } = useModalManager();
 const { createToast } = useToast();
 
-defineProps<{
+const props = defineProps<{
     routeForm: RouteFormDefinition<'post'>;
-    message: string;
+    content: string;
+    successMessage: string;
 }>();
+
+const errors = computed(() => usePage().props.errors);
+
+const handleError = () => {
+    closeModal();
+    createToast(errors.value.delete, 'error');
+};
 
 const handleSuccess = () => {
     closeModal();
-    createToast('Vault deleted', 'success');
+    createToast(props.successMessage, 'success');
 };
 </script>
 
@@ -29,9 +38,10 @@ const handleSuccess = () => {
             autocomplete="off"
             novalidate
             disable-while-processing
+            @error="handleError"
             @success="handleSuccess"
         >
-            <p>{{ message }}</p>
+            <p>{{ content }}</p>
             <div class="flex justify-end gap-2 pb-1">
                 <SecondaryButton @click="closeModal">Cancel</SecondaryButton>
                 <Submit label="Confirm" autofocus :processing="processing" />
