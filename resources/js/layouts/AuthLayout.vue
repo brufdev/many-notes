962c6042d64f2f2d97ec3e9acc99864b42e8b904
Bruno <brufdev@proton.me>
2025-12-08 19:36:40 +0000

    Add authenticated layout

---
+++ resources/js/layouts/AuthLayout.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import ModalManager from '@/components/modal/ModalManager.vue';
+import Toast from '@/components/toast/Toast.vue';
+import Spinner from '@/icons/Spinner.vue';
+import { useLoadingStore } from '@/stores/loading';
+import AppLayout from './AppLayout.vue';
+
+const loadingStore = useLoadingStore();
+</script>
+
+<template>
+    <AppLayout>
+        <header
+            class="bg-light-base-200 dark:bg-base-800 text-light-base-950 dark:text-base-50 border-light-base-300 dark:border-base-500 border-b print:hidden"
+        >
+            <div class="flex justify-between px-4 py-5">
+                <slot name="header" />
+            </div>
+        </header>
+
+        <main class="bg-light-base-50 dark:bg-base-900 relative flex flex-1 overflow-hidden">
+            <slot />
+        </main>
+
+        <div
+            v-if="loadingStore.appLoading"
+            class="bg-light-base-50 dark:bg-base-900 fixed inset-0 z-40 opacity-50"
+        >
+            <div class="flex h-full items-center justify-center">
+                <Spinner class="h-5 w-5 animate-spin" />
+            </div>
+        </div>
+
+        <ModalManager />
+
+        <Toast />
+    </AppLayout>
+</template>
