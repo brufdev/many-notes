53475840653ed900c601ecbc26fa0e1b24802809
brufdev <brufdev@proton.me>
2025-12-29 15:15:11 +0000

    Format type files with Prettier

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,7 +1,7 @@
+import type * as AxiosModule from 'axios';
 import type EchoClass from 'laravel-echo';
 import type { Broadcaster } from 'laravel-echo';
 import PusherClass from 'pusher-js';
-import type * as AxiosModule from 'axios';
 
 declare global {
     var Echo: EchoClass<keyof Broadcaster>;
--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,30 +1,30 @@
 export interface AppPageProps {
     app?: {
-        user?: User
-        settings?: Settings
-        metadata?: Metadata
-    }
+        user?: User;
+        settings?: Settings;
+        metadata?: Metadata;
+    };
 
-    [key: string]: any
+    [key: string]: any;
 }
 
 export interface User {
-    id: number
-    name: string
-    email: string
-    role: string
+    id: number;
+    name: string;
+    email: string;
+    role: string;
 }
 
 export interface Settings {
-    local_auth_enabled: boolean
-    registration: boolean
-    auto_update_check: boolean
+    local_auth_enabled: boolean;
+    registration: boolean;
+    auto_update_check: boolean;
 }
 
 export interface Metadata {
-    app_version: string
-    latest_version: string
-    github_url: string
-    update_available: boolean
-    upload_max_filesize: string
+    app_version: string;
+    latest_version: string;
+    github_url: string;
+    update_available: boolean;
+    upload_max_filesize: string;
 }
