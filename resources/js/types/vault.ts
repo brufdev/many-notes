523f0dc11955e537a3297b70ab6d34f97782ec82
brufdev <brufdev@proton.me>
2025-12-29 15:29:06 +0000

    Add vault node types

---
+++ resources/js/types/vault.ts
@@ -0,0 +1,18 @@
+export interface VaultNode {
+    id: number;
+    vault_id: number;
+    parent_id: number | null;
+    type: 'audio' | 'folder' | 'image' | 'note' | 'pdf' | 'video';
+    name: string;
+    extension: string | null;
+    content: string | null;
+    created_at: string;
+    updated_at: string;
+}
+
+export type VaultNodeTreeItem = Pick<
+    VaultNode,
+    'id' | 'parent_id' | 'type' | 'name' | 'extension'
+> & {
+    has_children: boolean;
+};
