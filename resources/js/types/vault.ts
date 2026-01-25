eff5ca0fd359b43e4d120b9bf663560e55a1e613
brufdev <brufdev@proton.me>
2026-01-25 18:22:46 +0000

    Add Vault types

--- resources/js/types/vault.ts
+++ resources/js/types/vault.ts
@@ -1,3 +1,32 @@
+import { User } from '.';
+
+export interface Vault {
+    id: number;
+    name: string;
+    templates_node_id: number | null;
+    user: VaultUser;
+    collaborators: VaultCollaborator[];
+    created_by: number;
+    updated_at: string;
+}
+
+export type VaultUser = Pick<User, 'id' | 'name' | 'email'>;
+
+export interface VaultCollaborator extends VaultUser {
+    accepted: boolean;
+}
+
+export type VaultListItem = Vault & {
+    accepted_collaborators_count: number;
+}
+
+export interface RecentVaultFile {
+    id: number;
+    name: string;
+    full_path: string;
+    time_elapsed: string;
+}
+
 export interface VaultNode {
     id: number;
     vault_id: number;
@@ -10,7 +39,4 @@ export interface VaultNode {
     updated_at: string;
 }
 
-export type VaultNodeTreeItem = Pick<
-    VaultNode,
-    'id' | 'parent_id' | 'type' | 'name' | 'extension'
->;
+export type VaultNodeTreeItem = Pick<VaultNode, 'id' | 'parent_id' | 'type' | 'name' | 'extension'>;
