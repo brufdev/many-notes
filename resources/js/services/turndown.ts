ea251360c15b34c5b858ce9fe079fd289f47648c
brufdev <brufdev@proton.me>
2026-04-11 00:28:03 +0100

    chore: migrate Turndown to Typescript

---
+++ resources/js/services/turndown.ts
@@ -0,0 +1,116 @@
+import {
+    highlightedCodeBlock,
+    strikethrough,
+    tables,
+    taskListItems,
+} from '@guyplusplus/turndown-plugin-gfm';
+import TurndownService from 'turndown';
+
+TurndownService.prototype.escape = (string: string): string => string;
+
+export const turndownService = new TurndownService({
+    headingStyle: 'atx',
+    hr: '---',
+    bulletListMarker: '-',
+    codeBlockStyle: 'fenced',
+    emDelimiter: '*',
+    linkReferenceStyle: 'shortcut',
+})
+    .use([highlightedCodeBlock, strikethrough, tables, taskListItems])
+    .addRule('listItem', {
+        filter: 'li',
+        replacement(content: string, node: Node, options: TurndownService.Options): string {
+            const element = node as HTMLLIElement;
+            content = content
+                .replace(/^\n+/, '') // Remove leading newlines
+                .replace(/\n+$/, '\n') // Replace trailing newlines with just a single one
+                .replace(/\n/gm, '\n    '); // Indent
+
+            let prefix = (options.bulletListMarker ?? '-') + ' ';
+            const parent = element.parentNode as HTMLElement | null;
+
+            if (parent?.nodeName === 'OL') {
+                const olParent = parent as HTMLOListElement;
+                const start = olParent.getAttribute('start');
+                const index = Array.prototype.indexOf.call(olParent.children, element);
+                prefix = (start ? Number(start) + index : index + 1) + '. ';
+            }
+
+            const isTaskList = content.startsWith('[ ]') || content.startsWith('[x]');
+
+            // Remove lines containing only whitespaces
+            if (!isTaskList) {
+                content = content.replace(/^\s+$\n?/gm, '');
+            }
+
+            return prefix + content + (element.nextSibling && !content.endsWith('\n') ? '\n' : '');
+        },
+    })
+    .addRule('image', {
+        filter: 'img',
+        replacement(content: string, node: Node): string {
+            const element = node as HTMLImageElement;
+            const alt = element.getAttribute('alt') ?? '';
+            const src = element.getAttribute('src')?.replace(/^\/files\/\d+\?path=/, '');
+            const title = element.getAttribute('title');
+            const titlePart = title ? ` "${title}"` : '';
+
+            if (!src) {
+                return content;
+            }
+
+            try {
+                return `![${alt}](${decodeURI(src)}${titlePart})`;
+            } catch {
+                return `![${alt}](${src}${titlePart})`;
+            }
+        },
+    })
+    .addRule('link', {
+        filter: 'a',
+        replacement(content: string, node: Node): string {
+            const element = node as HTMLAnchorElement;
+            const href = element.getAttribute('href');
+            const title = element.getAttribute('title');
+            const titlePart = title ? ` "${title}"` : '';
+            const autoLink = element.classList.contains('autoLink');
+            const angleBracketLink = element.dataset.angleBracket === 'true';
+
+            if (!href) {
+                return content;
+            }
+
+            let cleanHref: string;
+
+            try {
+                cleanHref = decodeURI(href);
+            } catch {
+                cleanHref = href;
+            }
+
+            cleanHref = cleanHref.replace(/^mailto:/, '');
+
+            if (autoLink) {
+                return cleanHref;
+            }
+
+            if (angleBracketLink) {
+                return `<${cleanHref}>`;
+            }
+
+            return `[${content}](${cleanHref}${titlePart})`;
+        },
+    })
+    .addRule('hashtag', {
+        filter: (node: Node): boolean => {
+            const element = node as HTMLElement;
+
+            return element.nodeName === 'SPAN' && element.dataset.hashtag === 'true';
+        },
+        replacement(content: string, node: Node): string {
+            const element = node as HTMLElement;
+            const escaped = element.dataset.escaped === 'true';
+
+            return escaped ? `\\${content}` : content;
+        },
+    });
