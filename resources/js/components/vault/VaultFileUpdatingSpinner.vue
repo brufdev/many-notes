ac9195eef0bbb27d6db9d4f23576cdcc494e9f31
brufdev <brufdev@proton.me>
2026-04-16 23:53:33 +0100

    feat: add file updating spinner component

---
+++ resources/js/components/vault/VaultFileUpdatingSpinner.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Spinner from '@/icons/Spinner.vue';
+import { useLayoutStore } from '@/stores/layout';
+
+const layoutStore = useLayoutStore();
+</script>
+
+<template>
+    <span v-if="layoutStore.isVaultNodeUpdating" class="flex items-center">
+        <Spinner class="h-4 w-4 animate-spin" />
+    </span>
+</template>
