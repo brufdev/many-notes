0a9fcc376bf34bf7dad1718d6d40d24fb019276e
brufdev <brufdev@proton.me>
2026-05-08 22:54:52 +0100

    feat: add side panel management to layout store

--- resources/js/stores/layout.ts
+++ resources/js/stores/layout.ts
@@ -10,6 +10,20 @@ export const useLayoutStore = defineStore('layout', () => {
         localStorage.getItem('contentWidthFull') === 'true' || false
     );
 
+    const isLeftPanelOpen = ref<boolean>(false);
+    const isRightPanelOpen = ref<boolean>(false);
+
+    const leftPanelPreferredOpen = ref<boolean>(
+        localStorage.getItem('leftPanelPreferredOpen')
+            ? localStorage.getItem('leftPanelPreferredOpen') === 'true'
+            : true
+    );
+    const rightPanelPreferredOpen = ref<boolean>(
+        localStorage.getItem('rightPanelPreferredOpen')
+            ? localStorage.getItem('rightPanelPreferredOpen') === 'true'
+            : true
+    );
+
     function setAppLoading(value: boolean) {
         isAppLoading.value = value;
     }
@@ -31,16 +45,57 @@ export const useLayoutStore = defineStore('layout', () => {
         localStorage.setItem('contentWidthFull', isContentWidthFull.value.toString());
     }
 
+    function toggleLeftPanel(isSmallScreen: boolean) {
+        if (!isSmallScreen) {
+            leftPanelPreferredOpen.value = !leftPanelPreferredOpen.value;
+            localStorage.setItem('leftPanelPreferredOpen', leftPanelPreferredOpen.value.toString());
+        }
+
+        isLeftPanelOpen.value = !isLeftPanelOpen.value;
+    }
+
+    function toggleRightPanel(isSmallScreen: boolean) {
+        if (!isSmallScreen) {
+            rightPanelPreferredOpen.value = !rightPanelPreferredOpen.value;
+            localStorage.setItem(
+                'rightPanelPreferredOpen',
+                rightPanelPreferredOpen.value.toString()
+            );
+        }
+
+        isRightPanelOpen.value = !isRightPanelOpen.value;
+    }
+
+    function closePanels() {
+        isLeftPanelOpen.value = false;
+        isRightPanelOpen.value = false;
+    }
+
+    function syncPanelsWithScreen(isSmallScreen: boolean) {
+        if (isSmallScreen) {
+            closePanels();
+        } else {
+            isLeftPanelOpen.value = leftPanelPreferredOpen.value;
+            isRightPanelOpen.value = rightPanelPreferredOpen.value;
+        }
+    }
+
     return {
         isAppLoading,
         isTreeViewLoading,
         isVaultNodeUpdating,
         isContentWidthFull,
         showToggleContentWidthButton,
+        isLeftPanelOpen,
+        isRightPanelOpen,
         setAppLoading,
         setTreeViewLoading,
         setVaultNodeUpdating,
         setShowToggleContentWidthButton,
         toggleContentWidth,
+        toggleLeftPanel,
+        toggleRightPanel,
+        closePanels,
+        syncPanelsWithScreen,
     };
 });
